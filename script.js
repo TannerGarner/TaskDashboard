15b9aa74c684f5c4ecb9c48eb2aeee76da3ff32b
tannergarner <garnert.w.14@gmail.com>
2024-10-14 09:28:06 -0600

    Completed addTask functionality for the toDo section

--- script.js
+++ script.js
@@ -1,44 +1,39 @@
-const addTaskTD = document.getElementById('addTaskTD')
 const toDo = document.getElementById('toDo')
+const addTaskTD = document.getElementById('addTaskTD')
+const addTaskIP = document.getElementById('addTaskIP')
+const addTaskNR = document.getElementById('addTaskNR')
+const addTaskDone = document.getElementById('addTaskDone')
+const inputAreaTD = document.getElementById('inputAreaTD')
+const inputTitleTD = document.getElementById('inputTitleTD')
+const inputDescriptionTD = document.getElementById('inputDescriptionTD')
+const inputSaveTD = document.getElementById('inputSaveTD')
 
 class Task {
-  constructor(id) {
-    this.title = this.title;
-    this.description = this.description;
+  constructor(title, description, section, id) {
+    this.title = title;
+    this.description = description;
+    this.section = section;
     this.items = this.items;
     this.users = this.users;
     this.id = id;
   }
 
-  createTask() {
-    //create input area
-    const inputArea = document.createElement('div')
-    inputArea.id = `inputArea${this.id}`;
-    inputArea.classList.add('card')
-    //create title
-    const inputTitle = document.createElement('input')
-    inputTitle.type = 'text';
-    inputTitle.id = `inputTitle${this.id}`;
-    //create description
-    const inputDescription = document.createElement('textarea')
-    inputDescription.id = `inputDescription${this.id}`;
-    //create save button
-    const inputSave = document.createElement('button')
-    inputSave.id - `inputSave${this.id}`
-    //append inputs to input area
-    inputArea.innerHTML += inputTitle;
-    inputArea.innerHTML += inputDescription;
-    inputArea.innerHTML += inputSave;
-    //return inputArea
-    return(inputArea)
-  }
-
   addTask() {
+    //task card
     const task = document.createElement('div');
-    task.classList.add('taskCard');
-    task.id.add(`task${this.index}`)
-    const taskTitle = document.createElement('h5')
-
+    task.classList.add('tasks');
+    task.classList.add('card');
+    task.id = (`task${this.section}${this.id}`);
+    //task content
+    const taskTitle = document.createElement('h4');
+    taskTitle.textContent = this.title;
+    const taskDescription = document.createElement('p');
+    taskDescription.textContent = this.description;
+    //append content to card
+    task.appendChild(taskTitle);
+    task.appendChild(taskDescription)
+    //append card to DOM
+    this.section.appendChild(task);
   }
 
   editTask() {
@@ -54,12 +49,44 @@ class Task {
   }
 }
 
-let inputId = 0;
+let idEnding;
 
 addTaskTD.addEventListener('click', function(){
-  let taskInput = new Task(inputId);
-  let newTaskInput = taskInput.createTask();
-  console.log(newTaskInput)
-  toDo.innerHTML += `${newTaskInput}`;
-  inputId ++
+  idEnding = 'TD'
+  toggleTaskInput(idEnding)
+})
+
+addTaskIP.addEventListener('click', function(){
+  idEnding = 'IP'
+  toggleTaskInput(idEnding)
+})
+
+addTaskNR.addEventListener('click', function(){
+  idEnding = 'NR'
+  toggleTaskInput(idEnding)
+})
+
+addTaskDone.addEventListener('click', function(){
+  idEnding = 'Done'
+  toggleTaskInput(idEnding)
+})
+
+function toggleTaskInput(idKey){
+  const inputArea = document.getElementById(`inputArea${idKey}`)
+  inputArea.classList.toggle('toggleDisplay')
+  document.getElementById(`addTask${idKey}`).classList.toggle('toggleDisplay')
+}
+
+let idTD = 0;
+
+inputSaveTD.addEventListener('click', function(){
+  let title = inputTitleTD.value;
+  let description = inputDescriptionTD.value;
+  let section = toDo;
+  let toDoTask = new Task(title, description, section, idTD);
+  idTD++ ;
+  console.log(toDoTask);
+  
+  toggleTaskInput('TD')
+  toDoTask.addTask();
 })
