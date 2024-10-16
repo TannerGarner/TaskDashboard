76787244db541722ed4765cd6b570be9a1f26fc7
tannergarner <garnert.w.14@gmail.com>
2024-10-16 10:36:06 -0600

    Complete functionality for adding and deleting tasks.

--- script.js
+++ script.js
@@ -1,92 +1,216 @@
-const toDo = document.getElementById('toDo')
-const addTaskTD = document.getElementById('addTaskTD')
-const addTaskIP = document.getElementById('addTaskIP')
-const addTaskNR = document.getElementById('addTaskNR')
-const addTaskDone = document.getElementById('addTaskDone')
-const inputAreaTD = document.getElementById('inputAreaTD')
-const inputTitleTD = document.getElementById('inputTitleTD')
-const inputDescriptionTD = document.getElementById('inputDescriptionTD')
-const inputSaveTD = document.getElementById('inputSaveTD')
+window.onload = loadTasks;
+
+const toDo = document.getElementById("toDo");
+const addTaskTD = document.getElementById("addTaskTD");
+const addTaskIP = document.getElementById("addTaskIP");
+const addTaskNR = document.getElementById("addTaskNR");
+const addTaskDone = document.getElementById("addTaskDone");
+const inputAreaTD = document.getElementById("inputAreaTD");
+const inputTitleTD = document.getElementById("inputTitleTD");
+const inputDescriptionTD = document.getElementById("inputDescriptionTD");
+const inputSaveTD = document.getElementById("inputSaveTD");
+const inputTitleIP = document.getElementById("inputTitleIP");
+const inputDescriptionIP = document.getElementById("inputDescriptionIP");
+const inputSaveIP = document.getElementById("inputSaveIP");
+const inputTitleNR = document.getElementById("inputTitleNR");
+const inputDescriptionNR = document.getElementById("inputDescriptionNR");
+const inputSaveNR = document.getElementById("inputSaveNR");
+const inputTitleDone = document.getElementById("inputTitleDone");
+const inputDescriptionDone = document.getElementById("inputDescriptionDone");
+const inputSaveDone = document.getElementById("inputSaveDone");
 
 class Task {
-  constructor(title, description, section, id) {
+  constructor(title, description, status, id) {
     this.title = title;
     this.description = description;
-    this.section = section;
+    this.status = status;
     this.items = this.items;
     this.users = this.users;
-    this.id = id;
+    this.id = id || Math.floor(Math.random() * 100000);
   }
 
   addTask() {
     //task card
-    const task = document.createElement('div');
-    task.classList.add('tasks');
-    task.classList.add('card');
-    task.id = (`task${this.section}${this.id}`);
+    const task = document.createElement("div");
+    task.classList.add("tasks", "card");
+    task.id = `task${this.id}`;
     //task content
-    const taskTitle = document.createElement('h4');
-    taskTitle.textContent = this.title;
-    const taskDescription = document.createElement('p');
+    const title = document.createElement("h4");
+    title.textContent = this.title;
+    //menu drop down
+    const menuButton = document.createElement("i");
+    menuButton.classList.add("fa-solid", "fa-ellipsis-vertical", "menuButton");
+    const menu = document.createElement("div");
+    menu.classList.add("toggleDisplay", "menu");
+    menu.id = `menu${this.id}`;
+    //menu content
+    const edit = document.createElement("p");
+    edit.textContent = "Edit Task";
+    edit.id = `edit${this.id}`;
+    const trash = document.createElement("p");
+    trash.textContent = "Discard Task";
+    menu.appendChild(edit);
+    menu.appendChild(trash);
+    const taskDescription = document.createElement("p");
     taskDescription.textContent = this.description;
     //append content to card
-    task.appendChild(taskTitle);
-    task.appendChild(taskDescription)
+    task.appendChild(title);
+    task.appendChild(menuButton);
+    task.appendChild(menu);
+    task.appendChild(taskDescription);
     //append card to DOM
-    this.section.appendChild(task);
+    document.getElementById(`${this.status}`).appendChild(task);
+    //Add event listener to menu button
+    menuButton.addEventListener("click", function () {
+      menu.classList.toggle("toggleDisplay");
+    });
+    //Add event listener to edit and delete buttons
+    edit.addEventListener("click", () => {
+      this.editTask(task);
+    });
+    trash.addEventListener("click", () => {
+      this.removeTask(task);
+    });
   }
 
-  editTask() {
-    
+  saveTask() {
+    let tasks = JSON.parse(localStorage.getItem(`tasks`)) || [];
+    tasks.push({
+      id: this.id,
+      status: this.status,
+      title: this.title,
+      description: this.description,
+    });
+
+    localStorage.setItem(`tasks`, JSON.stringify(tasks));
   }
 
-  removeTask() {
+  editTask() {}
 
+  removeTask(domElement) {
+    //remove from storage
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    const index = tasks.findIndex((taskData) => taskData.id === this.id);
+
+    if (index !== -1) {
+      tasks.splice(index, 1);
+    }
+    console.log(tasks);
+
+    localStorage.setItem(`tasks`, JSON.stringify(tasks));
+
+    //remove dom Element
+    if (domElement) {
+      domElement.remove();
+    }
   }
 
-  moveTask() {
-
-  }
+  moveTask() {}
 }
 
 let idEnding;
 
-addTaskTD.addEventListener('click', function(){
-  idEnding = 'TD'
-  toggleTaskInput(idEnding)
-})
+addTaskTD.addEventListener("click", function () {
+  idEnding = "TD";
+  toggleTaskInput(idEnding);
+});
 
-addTaskIP.addEventListener('click', function(){
-  idEnding = 'IP'
-  toggleTaskInput(idEnding)
-})
+addTaskIP.addEventListener("click", function () {
+  idEnding = "IP";
+  toggleTaskInput(idEnding);
+});
 
-addTaskNR.addEventListener('click', function(){
-  idEnding = 'NR'
-  toggleTaskInput(idEnding)
-})
+addTaskNR.addEventListener("click", function () {
+  idEnding = "NR";
+  toggleTaskInput(idEnding);
+});
 
-addTaskDone.addEventListener('click', function(){
-  idEnding = 'Done'
-  toggleTaskInput(idEnding)
-})
+addTaskDone.addEventListener("click", function () {
+  idEnding = "Done";
+  toggleTaskInput(idEnding);
+});
 
-function toggleTaskInput(idKey){
-  const inputArea = document.getElementById(`inputArea${idKey}`)
-  inputArea.classList.toggle('toggleDisplay')
-  document.getElementById(`addTask${idKey}`).classList.toggle('toggleDisplay')
+function toggleTaskInput(idKey) {
+  const inputArea = document.getElementById(`inputArea${idKey}`);
+  inputArea.classList.toggle("toggleDisplay");
+  document.getElementById(`addTask${idKey}`).classList.toggle("toggleDisplay");
 }
 
-let idTD = 0;
-
-inputSaveTD.addEventListener('click', function(){
+//Save Task by Section
+inputSaveTD.addEventListener("click", function () {
   let title = inputTitleTD.value;
   let description = inputDescriptionTD.value;
-  let section = toDo;
-  let toDoTask = new Task(title, description, section, idTD);
-  idTD++ ;
+  let status = "toDo";
+  let toDoTask = new Task(title, description, status);
   console.log(toDoTask);
-  
-  toggleTaskInput('TD')
+
+  toggleTaskInput("TD");
   toDoTask.addTask();
-})
+  toDoTask.saveTask();
+
+  //empty input area
+  inputTitleTD.value = "";
+  inputDescriptionTD.value = "";
+});
+
+inputSaveIP.addEventListener("click", function () {
+  let title = inputTitleIP.value;
+  let description = inputDescriptionIP.value;
+  let status = "inProgress";
+  let inProgressTask = new Task(title, description, status);
+  console.log(inProgressTask);
+
+  toggleTaskInput("IP");
+  inProgressTask.addTask();
+  inProgressTask.saveTask();
+
+  //empty input area
+  inputTitleIP.value = "";
+  inputDescriptionIP.value = "";
+});
+
+inputSaveNR.addEventListener("click", function () {
+  let title = inputTitleNR.value;
+  let description = inputDescriptionNR.value;
+  let status = "needsReview";
+  let needsReviewTask = new Task(title, description, status);
+  console.log(needsReviewTask);
+
+  toggleTaskInput("NR");
+  needsReviewTask.addTask();
+  needsReviewTask.saveTask();
+
+  //empty input area
+  inputTitleNR.value = "";
+  inputDescriptionNR.value = "";
+});
+
+inputSaveDone.addEventListener("click", function () {
+  let title = inputTitleDone.value;
+  let description = inputDescriptionDone.value;
+  let status = "done";
+  let doneTask = new Task(title, description, status);
+  console.log(doneTask);
+
+  toggleTaskInput("Done");
+  doneTask.addTask();
+  doneTask.saveTask();
+
+  //empty input area
+  inputTitleDone.value = "";
+  inputDescriptionDone.value = "";
+});
+
+function loadTasks() {
+  let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+  tasks.forEach((taskData) => {
+    let loadTask = new Task(
+      taskData.title,
+      taskData.description,
+      taskData.status,
+      taskData.id
+    );
+    loadTask.addTask();
+  });
+}
